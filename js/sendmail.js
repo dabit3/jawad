68328bb3c58f9bd0d86dd2c9f26f176c4b8f3a43
dabit3 <dabit3@gmail.com>
2015-03-10 22:24:08 -0500

    Added signup for Startup weekend

--- js/sendmail.js
+++ js/sendmail.js
@@ -30,4 +30,27 @@ $(document).ready(function() {
 	        }
     });
 
+		$('#mailinglist2').click(function () {
+	        
+	        var email = $('#updates-email').val();
+	        var varData = 'email=' + email;
+	        if (email == "") {
+	    			swal("Please fill out the email field.");
+	    			return false;
+	    		}
+	        
+	        if(email != "") {
+	        		swal("You've signed up to be sponsored for Startup Weekend! ", "We will update you shortly.", "success");
+	            
+	        		emailRef.push({ email: email });
+	            console.log(email);
+
+	            $('#updates-email').val("");
+	            
+
+	            return false;
+	        }
+    });
+
+
 });
